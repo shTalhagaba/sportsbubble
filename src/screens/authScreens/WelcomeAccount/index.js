f42288799db7f5e94514f5b8aec166b0d3f42dd3
Abdullah Shahid <48951700+abdulllahshahid@users.noreply.github.com>
2023-09-02 16:59:50 +0500

    Signup

--- src/screens/authScreens/WelcomeAccount/index.js
+++ src/screens/authScreens/WelcomeAccount/index.js
@@ -41,7 +41,7 @@ export default function WelcomeAccount(props) {
   const zipCodeRef = useRef();
   const birthdayRef = useRef();
   const pronounsRef = useRef();
-  const [dob, setDOB] = useState(new Date());
+  const [dob, setDOB] = useState('');
   const [showDatePicker, setShowDatePicker] = useState(false);
 
   const options = [
@@ -96,7 +96,7 @@ export default function WelcomeAccount(props) {
     console.log("Date ", date)
     // const selectedDate = moment(date).format('DD MMMM YYYY');
     setDate(date)
-    setDOB(date)
+    setDOB(date.toDateString())
     hideDatePicker();
   };
 
@@ -104,134 +104,137 @@ export default function WelcomeAccount(props) {
   minimumDOB.setFullYear(minimumDOB.getFullYear() - 14); // Minimum age of 14 years
 
   return (
-    <ImageBackground
-      source={Images.Background2}
-      resizeMode="contain"
-      style={styles.container}>
-      <StatusBar
-        backgroundColor={Colors.transparent}
-        translucent
-        barStyle="light-content"
-      />
-      <AppHeader
-        centerImage={Images.Logo}
-        LeftImage={Images.LeftIcon}
-        headerContainer={{ marginTop: 10 }}
-        SimpleView
-      />
-      <KeyboardAwareScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ flexGrow: 1, paddingBottom: 30 }}>
-        <View style={styles.innerContainer}>
-          <Text style={styles.welcomeTxt}>
-            {Strings.welcomeName.replace('__NAME__', firstName)}
-          </Text>
-          <Text style={styles.accountTxt}>{Strings.weJustNeed}</Text>
-          {/* Zip Code Input */}
-          <ContactTextInput
-            leftImage={Images.Location}
-            refInner={zipCodeRef}
-            Container={{ marginTop: 30 }}
-            placeholderTextColor={Colors.white}
-            placeholder={Strings.zipCode}
-            multiline={false}
-            value={zipCode}
-            maxLength={6}
-            onChangeText={txt => setZipCode(txt)}
-            keyboardType={'default'}
-            autoCapitalize="none"
-            returnKeyType={'next'}
-            blurOnSubmit={false}
-            onSubmitEditing={() => {
-              birthdayRef.current.focus();
-            }}
-          />
-          <Text style={styles.sideTxt}>{Strings.sharingYourLocation}</Text>
-          {/* Date of Birth Input */}
-          <ContactTextInput
-            leftImage={Images.Birthday}
-            refInner={birthdayRef}
-            Container={{ marginTop: 30 }}
-            placeholderTextColor={Colors.white}
-            placeholder={Strings.birthdate}
-            multiline={false}
-            editable={false}
-            value={dob.toDateString()}
-            maxLength={50}
-            onChangeText={txt => setBirthday(txt)}
-            keyboardType={'default'}
-            autoCapitalize="none"
-            returnKeyType={'next'}
-            blurOnSubmit={false}
-            rightImage={Images.Calendar}
-            pressRightImage={() => handleShowDatePicker()}
-            // pressRightImage={() => setShowDatePicker(!showDatePicker)}
-            onSubmitEditing={() => {
-              pronounsRef.current.focus();
-            }}
-          />
+    <View style={{ flex: 1, backgroundColor: Colors.appColorBackground, }}>
+      <ImageBackground
+        source={Images.Background2}
+        resizeMode="cover"
+        style={styles.container}>
+        <StatusBar
+          backgroundColor={Colors.transparent}
+          translucent
+          barStyle="light-content"
+        />
+        <AppHeader
+          centerImage={Images.Logo}
+          LeftImage={Images.LeftIcon}
+          headerContainer={{ marginTop: 10 }}
+          SimpleView
+        />
+        <KeyboardAwareScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ flexGrow: 1, paddingBottom: 30 }}>
+          <View style={styles.innerContainer}>
+            <Text style={styles.welcomeTxt}>
+              {Strings.welcomeName.replace('__NAME__', firstName)}
+            </Text>
+            <Text style={styles.accountTxt}>{Strings.weJustNeed}</Text>
+            {/* Zip Code Input */}
+            <ContactTextInput
+              leftImage={Images.Location}
+              refInner={zipCodeRef}
+              Container={{ marginTop: 30 }}
+              placeholderTextColor={Colors.white}
+              placeholder={Strings.zipCode}
+              multiline={false}
+              value={zipCode}
+              maxLength={6}
+              onChangeText={txt => setZipCode(txt)}
+              keyboardType={'default'}
+              autoCapitalize="none"
+              returnKeyType={'next'}
+              blurOnSubmit={false}
+              onSubmitEditing={() => {
+                birthdayRef.current.focus();
+              }}
+            />
+            <Text style={styles.sideTxt}>{Strings.sharingYourLocation}</Text>
+            {/* Date of Birth Input */}
+            <ContactTextInput
+              leftImage={Images.Birthday}
+              refInner={birthdayRef}
+              Container={{ marginTop: 30 }}
+              placeholderTextColor={Colors.white}
+              placeholder={Strings.birthdate}
+              multiline={false}
+              editable={false}
+              value={dob}
+              maxLength={50}
+              onChangeText={txt => setBirthday(txt)}
+              keyboardType={'default'}
+              autoCapitalize="none"
+              returnKeyType={'next'}
+              blurOnSubmit={false}
+              rightImage={Images.Calendar}
+              pressRightImage={() => handleShowDatePicker()}
+              // pressRightImage={() => setShowDatePicker(!showDatePicker)}
+              onSubmitEditing={() => {
+                pronounsRef.current.focus();
+              }}
+            />
 
-          <Text style={styles.sideTxt}>{Strings.youMustBe}</Text>
-          {/* Pronouns Input */}
-          <ContactTextInput
-            leftImage={Images.Pronouns}
-            Container={{ marginTop: 30 }}
-            refInner={pronounsRef}
-            placeholderTextColor={Colors.white}
-            placeholder={Strings.pronouns}
-            multiline={false}
-            editable={false}
-            value={pronouns}
-            maxLength={50}
-            onChangeText={txt => setPronouns(txt)}
-            keyboardType={'default'}
-            autoCapitalize="none"
-            returnKeyType={'done'}
-            rightImage={Images.DownArrow}
-            pressRightImage={toggleDropdown}
-          />
-          {isOpen && (
-            <View
-              style={{
-                position: 'absolute',
-                bottom: 1,
-                zIndex: 999,
-                width: '98%',
-              }}>
-              <FlatList
-                data={options}
-                keyExtractor={item => item.id.toString()}
-                renderItem={({ item }) => (
-                  <TouchableOpacity
-                    onPress={() => handleSelect(item)}
-                    style={styles.dropdownItem}>
-                    <Text style={styles.itemTitle}>{item.label}</Text>
-                  </TouchableOpacity>
-                )}
+            <Text style={styles.sideTxt}>{Strings.youMustBe}</Text>
+            {/* Pronouns Input */}
+            <ContactTextInput
+              leftImage={Images.Pronouns}
+              Container={{ marginTop: 30 }}
+              refInner={pronounsRef}
+              placeholderTextColor={Colors.white}
+              placeholder={Strings.pronouns}
+              multiline={false}
+              editable={false}
+              value={pronouns}
+              maxLength={50}
+              onChangeText={txt => setPronouns(txt)}
+              keyboardType={'default'}
+              autoCapitalize="none"
+              returnKeyType={'done'}
+              rightImage={Images.DownArrow}
+              pressRightImage={toggleDropdown}
+            />
+            {isOpen && (
+              <View
+                style={{
+                  position: 'absolute',
+                  bottom: 1,
+                  zIndex: 999,
+                  width: '98%',
+                }}>
+                <FlatList
+                  data={options}
+                  keyExtractor={item => item.id.toString()}
+                  renderItem={({ item }) => (
+                    <TouchableOpacity
+                      onPress={() => handleSelect(item)}
+                      style={styles.dropdownItem}>
+                      <Text style={styles.itemTitle}>{item.label}</Text>
+                    </TouchableOpacity>
+                  )}
+                />
+              </View>
+            )}
+            <Text style={styles.sideTxt}>{Strings.wordingForThisTk}</Text>
+            <View style={styles.btnContainer}>
+              <CustomButton
+                title={Strings.continue}
+                Container={styles.blueButtonContainer}
+                txt={styles.blueButtonTxt}
+                onpress={() => submitButton()}
               />
             </View>
-          )}
-          <Text style={styles.sideTxt}>{Strings.wordingForThisTk}</Text>
-          <View style={styles.btnContainer}>
-            <CustomButton
-              title={Strings.continue}
-              Container={styles.blueButtonContainer}
-              txt={styles.blueButtonTxt}
-              onpress={() => submitButton()}
-            />
           </View>
-        </View>
-      </KeyboardAwareScrollView>
-      <LoaderModal visible={loadingLocal} loadingText={''} />
-      <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        mode="date"
-        onConfirm={handleConfirm}
-        onCancel={hideDatePicker}
-        maximumDate={minimumDOB}
-      // maximumDate={new Date()}
-      // minimumDOB={minimumDOB}
-      />
-    </ImageBackground>
+        </KeyboardAwareScrollView>
+        <LoaderModal visible={loadingLocal} loadingText={''} />
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+          maximumDate={minimumDOB}
+        // maximumDate={new Date()}
+        // minimumDOB={minimumDOB}
+        />
+      </ImageBackground>
+    </View>
+
   );
 }
