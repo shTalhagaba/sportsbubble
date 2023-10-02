55b84b5d056cf0cfbcb70c85049f476edf714fb0
Talha <54505302+shTalhagaba@users.noreply.github.com>
2023-10-02 21:09:10 +0500

    changes

--- src/components/SvgRenderer/index.js
+++ src/components/SvgRenderer/index.js
@@ -1,41 +1,86 @@
+import { Colors } from 'src/utils';
 import React from 'react';
-import { View } from 'react-native';
+import { Dimensions, View } from 'react-native';
 import WebView from 'react-native-webview';
-import styles from './styles';
+const screenWidth = Dimensions.get('window').width;
 
-const SvgRenderer = ({ url, flag }) => {
+const SvgRenderer = ({ url, flag, width, height }) => {
     return (
         flag ?
-            <View style={styles.container}>
+            <View style={{
+                width: width ? width : screenWidth / 2.5,
+                height: height ? height : screenWidth / 4,
+            }}>
                 <WebView
                     source={{
                         html: `
-            <html style="background:transparent;">
-            <body>
-              <div style="height:100vh; width:100vw; background:transparent;">
-                <img src="${url}" height="100%" width="100%" style="background:transparent;" />
-              </div>
-            </body>
-            </html>`
+                        <html style="background:transparent;">
+                        <body>
+                        <div style="height:100vh; width:100vw; background:transparent;">
+                            <img src="${url}" height="100%" width="100%" style="background:transparent;" />
+                        </div>
+                        </body>
+                        </html>`
                     }}
-                    style={styles.innerContainer}
-                    containerStyle={styles.webContainer} />
+                    style={{
+                        width: width ? width : screenWidth / 2.5,
+                        height: height ? height : screenWidth / 4,
+                        backgroundColor: 'transparent', // Set background to transparent
+                    }}
+                    containerStyle={{
+                        borderRadius: 16,
+                        overflow: 'hidden',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        padding: 15,
+                        backgroundColor: 'transparent',
+                    }}
+                    scrollEnabled={false} // Disable scrolling
+                />
             </View> :
-            <View style={styles.webMainContainer}>
-                <View style={styles.webInnerContainer} />
+            <View style={{
+                backgroundColor: Colors.mediumBlue,
+                borderWidth: 2,
+                borderRadius: 16,
+                overflow: 'hidden',
+                borderColor: Colors.mediumBlue,
+                justifyContent: 'center',
+                width: screenWidth / 4.6,
+                height: screenWidth / 4.6,
+            }}>
+                <View style={{
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: Colors.black15,
+                }} />
                 <WebView
                     source={{
                         html: `
-            <html style="background:#3F5B80;">
-            <body>
-              <div style="height:100vh; width:100vw; background:#3F5B80;">
-                <img src="${url}" height="100%" width="100%" style="background:#3F5B80;" />
-              </div>
-            </body>
-            </html>`
+                        <html style="background:#3F5B80;">
+                        <body>
+                        <div style="height:100vh; width:100vw; background:#3F5B80;">
+                            <img src="${url}" height="100%" width="100%" style="background:#3F5B80;" />
+                        </div>
+                        </body>
+                        </html>`
                     }}
-                    style={styles.htmlContainer}
-                    containerStyle={styles.htmlInnerContainer}
+                    style={{
+                        width: screenWidth / 6,
+                        height: screenWidth / 6,
+                        backgroundColor: 'transparent', // Set background to transparent
+                    }}
+                    containerStyle={{
+                        borderRadius: 16,
+                        overflow: 'hidden',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        padding: 15,
+                        backgroundColor: Colors.mediumBlue,
+                    }}
+                    scrollEnabled={false} // Disable scrolling
                 />
             </View>
     );
