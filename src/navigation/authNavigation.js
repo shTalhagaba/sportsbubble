9a8670b13b95a39ad47552e04b70a08545a84e47
Abdullah Shahid <48951700+abdulllahshahid@users.noreply.github.com>
2023-10-13 10:26:51 +0500

    kill app no done

--- src/navigation/authNavigation.js
+++ src/navigation/authNavigation.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AppState } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Signup from 'src/screens/authScreens/Signup';
 import Login from 'src/screens/authScreens/Login';
@@ -7,19 +8,27 @@ import WelcomeAccount from 'src/screens/authScreens/WelcomeAccount';
 import WelcomeScreen from 'src/screens/authScreens/WelcomeScreen';
 import ResetPassword from 'src/screens/authScreens/ResetPassword';
 import Term from 'src/screens/appScreens/Term';
-import { useSelector } from 'react-redux';
+import { setUserVerifiedFlag, setUserEmail } from 'src/store/types';
+import { useDispatch, useSelector } from 'react-redux';
 
 const AuthNavigator = createNativeStackNavigator();
 
 const AuthNavigation = () => {
+    const dispatch = useDispatch();
+
+    const [appState, setAppState] = useState(AppState.currentState);
     const reduxData = useSelector(state => state.user);
     const memoizedReduxData = React.useMemo(() => reduxData, [reduxData]);
-    // Determine the initial route name based on memoizedReduxData
+
+
     const initialRouteName = memoizedReduxData?.userVerified === null
         ? "WelcomeScreen"
         : memoizedReduxData?.userVerified === false
             ? "Signup"
             : "WelcomeScreen";
+    console.log("initialRouteName =>", initialRouteName)
+    console.log("memoizedReduxData?.userVerified =>", memoizedReduxData?.userVerified)
+
 
     return (
         <AuthNavigator.Navigator
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useEffect, useState } from 'react';
+import { AppState } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthNavigator from './authNavigation';
 import MyTabs from './bottomTab'
@@ -8,6 +9,33 @@ import Splash from "src/screens/authScreens/Splash";
 const StackNavigator = createNativeStackNavigator()
 
 const AppStackNavigator = () => {
+  const [appState, setAppState] = useState(AppState.currentState);
+
+
+  useEffect(() => {
+    const handleAppStateChange = (nextAppState) => {
+      if (nextAppState === 'active') {
+        console.log('App is in the foreground');
+      } else if (nextAppState === 'inactive') {
+        console.log('App is being killed');
+        dispatch(setUserVerifiedFlag(null));
+        dispatch(setUserEmail(''));
+      } else {
+        if (nextAppState === "background") {
+          console.log('App is in the background');
+        } else {
+          console.log('App is in the KIll inactive');
+          dispatch(setUserVerifiedFlag(null));
+          dispatch(setUserEmail(''));
+        }
+      }
+    };
+
+    const appStateSubscription = AppState.addEventListener('change', handleAppStateChange);
+    return () => {
+      appStateSubscription.remove();
+    };
+  }, []);
   return (
     <StackNavigator.Navigator>
       <StackNavigator.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
@@ -16,9 +44,9 @@ const AppStackNavigator = () => {
       </StackNavigator.Screen>
       {/* Auth Navigator: Include Login and Signup */}
       <StackNavigator.Screen
-        name="Auth"
         component={AuthNavigator}
         options={{ headerShown: false }}
+        name="Auth"
       />
       <StackNavigator.Screen
         name="withoutBottomtab"
