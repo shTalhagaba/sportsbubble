8630ba5ab32450143ffba8ec6faba876a85d1ac4
Abdullah Shahid <48951700+abdulllahshahid@users.noreply.github.com>
2023-10-24 13:13:17 +0500

    554 app header size

--- src/components/AppHeader/styles.js
+++ src/components/AppHeader/styles.js
@@ -33,10 +33,10 @@ const styles = ScaledSheet.create({
             is_zoomed ? '40@ms0.3' : '24@ms0.3'
             : Platform.OS === "android" ?
                 fontScale > 1 ?
-                    24 * fontScale : "24@ms0.3" : "24@ms0.3",
+                    28 * fontScale : "24@ms0.3" : "24@ms0.3",
         height: is_zoomed ? '40@ms0.3' : Platform.OS === "android" ?
             fontScale > 1 ?
-                24 * fontScale : "24@ms0.3" : "24@ms0.3",
+                28 * fontScale : "24@ms0.3" : "24@ms0.3",
         resizeMode: 'cover',
         marginRight: Platform.OS === 'ios' ? '25@ms0.3' : '12@ms0.3',
     },
@@ -57,14 +57,14 @@ const styles = ScaledSheet.create({
                     : '24@ms0.3'
                 : Platform.OS === "android" ?
                     fontScale > 1 ?
-                        26 * fontScale : '24@ms0.3'
+                        33 * fontScale : '24@ms0.3'
                     : "24@ms0.3",
         width:
             Platform.OS === "ios" ?
                 is_zoomed ? '230@ms0.3' : '134@ms0.3' :
                 Platform.OS === "android" ?
                     fontScale > 1 ?
-                        130 * fontScale : '134@ms0.3' : '134@ms0.3',
+                        138 * fontScale : '134@ms0.3' : '134@ms0.3',
     },
     iconContainer: {
         width: '25%',
