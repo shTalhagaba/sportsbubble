64ded30a8c387895a46edbf19a1e0a1b49929378
Abdullah Shahid <48951700+abdulllahshahid@users.noreply.github.com>
2023-11-11 01:39:32 +0500

    svg zoom scroll issue

--- src/components/SvgRenderer/index.js
+++ src/components/SvgRenderer/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { Dimensions, Platform, View } from 'react-native';
 import WebView from 'react-native-webview';
 import styles from './styles';
+
 const screenWidth = Dimensions.get('window').width;
 
 const SvgRenderer = ({ url, flag, width, height }) => {
+  const viewportMetaTag =
+    Platform.OS === 'android'
+      ? '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />'
+      : '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />';
+
   return flag ? (
     <View
       style={{
@@ -13,25 +19,18 @@ const SvgRenderer = ({ url, flag, width, height }) => {
       }}>
       <WebView
         source={{
-          html:
-            Platform.OS === 'android'
-              ? `
-              <html>
-              <body style="background: transparent; margin-left: 12vw; margin-top: 7vw; padding: 0; justify-content: center; align-items: center; height: 100vh; overflow: hidden;">
-                <div style="height: 40vh; width: 40vw; background: transparent;">
+          html: `
+            <html>
+              <head>
+                ${viewportMetaTag}
+              </head>
+              <body style="background: transparent; margin-left: 20vw; margin-top: 7vw; padding: 0; justify-content: center; align-items: center; height: 100vh; overflow: hidden;">
+                <div style="height: 60vh; width: 60vw; background: transparent;">
                   <img src="${url}" height="100%" width="100%" style="background: transparent; display: block;"/>
                 </div>
               </body>
-            </html>                         
-            `
-              : `
-                        <html style="background:transparent;">
-                        <body>
-                        <div style="height:100vh; width:100vw; background:transparent;">
-                            <img src="${url}" height="100%" width="100%" style="background:transparent;" />
-                        </div>
-                        </body>
-                        </html>`,
+            </html>
+          `,
         }}
         style={styles.innerContainer}
         containerStyle={styles.webContainer}
@@ -43,25 +42,18 @@ const SvgRenderer = ({ url, flag, width, height }) => {
       <View style={styles.webInnerContainer} />
       <WebView
         source={{
-          html:
-            Platform.OS === 'android'
-              ? `
-              <html style="height: 100%; overflow: hidden;">
+          html: `
+            <html>
+              <head>
+                ${viewportMetaTag}
+              </head>
               <body style="background: #3F5B80; margin-left: 1vw; padding: 0; justify-content: center; align-items: center; height: 100vh; overflow: hidden;">
                 <div style="height: 25vh; width: 25vw; background: #3F5B80; display: flex; align-items: center; justify-content: center;">
                   <img src="${url}" height="100%" width="100%" style="background: transparent; object-fit: contain;" />
                 </div>
               </body>
             </html>
-            `
-              : `
-                    <html style="background:#3F5B80;">
-                    <body>
-                      <div style="height:90vh; width:90vw; background:#3F5B80; display: flex; align-items: center; justify-content: center;">
-                        <img src="${url}" style="width: 100%; height: 100%; object-fit: contain; background:#3F5B80;" />
-                      </div>
-                    </body>
-                    </html>`,
+          `,
         }}
         style={styles.htmlContainer}
         containerStyle={styles.htmlInnerContainer}
