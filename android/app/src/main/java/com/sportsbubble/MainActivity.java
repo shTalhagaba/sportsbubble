68f23a9264364971f59e82920406353ee91b6279
Talha Sadiq <54505302+shTalhagaba@users.noreply.github.com>
2024-01-20 01:57:47 +0500

    fixes

--- android/app/src/main/java/com/sportsbubble/MainActivity.java
+++ android/app/src/main/java/com/sportsbubble/MainActivity.java
@@ -1,4 +1,4 @@
-package com.watchsports;
+package com.watchsportsapp;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/sportsbubble/MainApplication.java
+++ android/app/src/main/java/com/sportsbubble/MainApplication.java
@@ -1,4 +1,4 @@
-package com.watchsports;
+package com.watchsportsapp;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
