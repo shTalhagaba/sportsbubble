0ad2e13e6163abc75a0872cdb68bfbb53b84d235
Abdullah Shahid <48951700+abdulllahshahid@users.noreply.github.com>
2024-01-20 00:10:31 +0500

    ios firebase

--- ios/sportsbubble/AppDelegate.mm
+++ ios/sportsbubble/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
 #import <RNPusherPushNotifications.h>
 
@@ -7,6 +7,7 @@
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
  self.moduleName = @"sportsbubble";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
